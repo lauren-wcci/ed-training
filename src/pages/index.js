8487680c25ccd3fca7077d3c569921c04b91b8d5
WCCI Instructor <lauren@wecancodeit.org>
2020-12-14 20:51:11 -0500

    added feature sections of index

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,10 +20,94 @@ const IndexPage = () => (
                 <img alt={"Feature Image"} src={featureImage}/>
             </div>
         </div>
-            <div className={"get-started"}>
-                <a href={"https://app.histaff.io"} target={"_blank"}>Start Training</a>
+        <div className={"container"}>
+            <div className={"features"}>
+                <div className={"feature__item"}>
+                    <div className={"row"}>
+                        <div className={"col-6 first"}>
+                            <div className={"thumbnail"}>
+                                <img alt={"Event"} src={thumbnailEvent}/>
+                            </div>
+                        </div>
+
+                        <div className={"col-6"}>
+                            <div className={"feature__content"}>
+                                <h2>Create and Join Events</h2>
+                                <p>Save your time and energy by letting HiStaff to manage your events (handle the colleague’s birthday, gathering, etc.)</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className={"feature__item"}>
+                    <div className={"row"}>
+                        <div className={"col-6"}>
+                            <div className={"feature__content"}>
+                                <h2>Public Board</h2>
+                                <p>Let the staff write down on the board freely,<br/>Joking with each other and writing cool content can make the workplace more diverse and attractive for employees. </p>
+                            </div>
+                        </div>
+
+                        <div className={"col-6 first"}>
+                            <div className={"thumbnail"}>
+                                <img alt={"Board"} src={thumbnailBoard}/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className={"feature__item"}>
+                    <div className={"row"}>
+                        <div className={"col-6 first"}>
+                            <div className={"thumbnail"}>
+                                <img alt={"News"} src={thumbnailNews}/>
+                            </div>
+                        </div>
+
+                        <div className={"col-6"}>
+                            <div className={"feature__content"}>
+                                <h2>Internal News</h2>
+                                <p>Get rid of sending news by e-mail and its problems; post the news in an internal and attractive format via HiStaff instead.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className={"feature__item"}>
+                    <div className={"row"}>
+                        <div className={"col-6"}>
+                            <div className={"feature__content"}>
+                                <h2>Teams</h2>
+                                <p>HiStaff let you add staff in specific teams and manage them easily.</p>
+                            </div>
+                        </div>
+
+                        <div className={"col-6 first"}>
+                            <div className={"thumbnail"}>
+                                <img alt={"Team"} src={thumbnailTeams}/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className={"feature__item"}>
+                    <div className={"row"}>
+                        <div className={"col-6 first"}>
+                            <div className={"thumbnail"}>
+                                <img alt={"Users"} src={thumbnailStaff}/>
+                            </div>
+                        </div>
+
+                        <div className={"col-6"}>
+                            <div className={"feature__content"}>
+                                <h2>Staff management</h2>
+                                <p>Discard traditional ways to archive staff documents and information, and try new ways to store and archive them in HiStaff easily.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            {/* <img alt={"Dashboard"} src={featureImage}/> */}
+        </div>
     </Layout>
 )
 
